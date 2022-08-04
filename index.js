eca71a0b92224194bea858da8ae3712d337e58c6
TSRodgers <tyler.rodgers74@gmail.com>
2022-08-04 13:34:21 -0400

    added questions and markdown

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: 'input',
+    message: 'What is your github username?',
+    name: 'username',
+  },
+  {
+    type: 'input',
+    message: 'What is your email?',
+    name: 'email',
+  },
+  {
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'title',
+  },
+  {
+    type: 'input',
+    message: 'Write a brief description of your project.',
+    name: 'description',
+  },
+  {
+    type: 'input',
+    message: 'What are the installation steps?',
+    name: 'install',
+  },
+  {
+    type: 'input',
+    message: 'How do you use this project?',
+    name: 'usage',
+  },
+  {
+    type: 'list',
+    message: 'What license should your project have?',
+    name: 'license',
+    choices: ['MIT', 'GNU', 'Apache'],
+    default: 'MIT',
+  },
+  {
+    type: 'input',
+    message: 'How do you test this project?',
+    name: 'test',
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions)
+    .then(response => {
+      console.log(response)
+    })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,8 +12,46 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+    ${data.description}
+
+    ${data.licenseBadge}
+
+
+---
+## About
+  ${data.about}
+
+---
+## Installation:
+  ${data.install}
+
+---
+## License
+  License used for this project - ${data.license}
+
+---
+## Contributing:
+  
+  To contribute to this application, create a pull request.
+  Here are the steps needed for doing that:
+  - Fork the repo
+  - Create a feature branch (git checkout -b NAME-HERE)
+  - Commit your new feature (git commit -m 'Add some feature')
+  - Push your branch (git push)
+  - Create a new Pull Request
+  Following a code review, your feature will be merged.
+
+---
+## Tests:
+  ${data.test}
+
+---
+## Contact Information:
+* GitHub Username: ${data.username}
+* GitHub Email: ${data.email}
 `;
 }
 
